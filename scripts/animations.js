beb34c1c3acbc7280adf6dc411bd7cb518880d2a
werer9 <werer9@gmail.com>
2015-03-26 13:35:17 +1300

    Added new divs

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
 		}, 500);
 		$(".hidden").fadeIn(500).removeClass('hidden');
 	}, 500);
+
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $($(this).attr('href'));
+
+    	if(target.length) {
+        	event.preventDefault();
+        	$('html, body').animate({
+            	scrollTop: target.offset().top - 80
+        	}, 500);
+   		}
+	});
 });
