83e107336165ca81bb6dbfe9d6973f3f9d842fb3
Caelan Murch <werer9@gmail.com>
2018-06-30 17:44:39 +0800

    Added navbar, animations and about section. Still work in progress

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    //change navbar colour on scroll
+    $(document).scroll(function () {
+	    var $nav = $("#navbar");
+	    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+
+    //scroll animation on click on navlink
+    $('a').click(function() {
+        var link = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 500);
+    });
+});
